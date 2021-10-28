ba97f9245cf0dd10f59e87a3994fc352742d7df1
Sixtus Anyanwu <sikky606@gmail.com>
2021-10-28 09:53:43 +0100

    added a transaction component

---
+++ src/components/Transaction.js
@@ -0,0 +1,12 @@
+const Transaction = ({transaction}) => {
+    // Check the sign of the amount and add it in front of the jsx amount
+    const sign = transaction.amount < 0 ? "-" : "+"
+  return (
+    <li className="minus">
+      {transaction.text} <span>{sign} $ {Math.abs(transaction.amount)} </span>
+      <button className="delete-btn">x</button>
+    </li>
+  );
+};
+
+export default Transaction;
--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -1,5 +1,6 @@
 import { GlobalContext } from "../context/GlobalState";
 import { useContext } from "react";
+import Transaction from "./Transaction"
 
 const TransactionList = () => {
   const { transactions } = useContext(GlobalContext); // Use destruc to pull out transactions from the global state
@@ -8,12 +9,7 @@ const TransactionList = () => {
     <div>
       <h3>History</h3>
       <ul id="list" className="list">
-        {transactions.map((transaction) => (
-          <li className="minus">
-            {transaction.text} <span>-$400</span>
-            <button className="delete-btn">x</button>
-          </li>
-        ))}
+        {transactions.map((transaction) => <Transaction key = { transaction.id } transaction = {transaction} /> )}
       </ul>
     </div>
   );
