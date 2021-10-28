3fc1abf30d0d9793700f5d9c8a77bd8093b2c52b
Sixtus Anyanwu <sikky606@gmail.com>
2021-10-28 17:04:12 +0100

    added an image

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 ### A simple expenses tracker app to handle your little expenses.
 Built with love and React.
+![alt text](public/exp_tracker.PNG?raw=true "Expense Tracker")
--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -33,7 +33,6 @@ const AddTransaction = () => {
         <div className="form-control">
           <label htmlFor="amount">
             Amount <br />
-            (negative - expense, positive - income)
           </label>
           <input type="number" value = {amount} 
           onChange = {(e) => setAmount(e.target.value)} placeholder="Enter amount..." />
